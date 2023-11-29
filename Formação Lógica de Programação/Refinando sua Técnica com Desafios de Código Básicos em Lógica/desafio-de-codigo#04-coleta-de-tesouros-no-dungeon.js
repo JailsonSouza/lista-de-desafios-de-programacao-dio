82de938bc0e7f1b5d49797cfcae44d9fac70cee2
Jailson Souza Carvalho <78353254+JailsonSouza@users.noreply.github.com>
2023-11-29 08:59:33 -0300

    uploading the codes

--- Formação Lógica de Programação/Refinando sua Técnica com Desafios de Código Básicos em Lógica/desafio-de-codigo#04-coleta-de-tesouros-no-dungeon.js
+++ Formação Lógica de Programação/Refinando sua Técnica com Desafios de Código Básicos em Lógica/desafio-de-codigo#04-coleta-de-tesouros-no-dungeon.js
@@ -3,21 +3,21 @@
 //- "print": imprime um texto de saída (output), pulando linha.
 
 // Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
-let escolhaDoTreinador = parseInt(gets());
-let pokemonEscolhido; 
+const totalSalas = parseInt(gets());
+//Aqui temos uma demonstração de array com valores já predefinidos:
+const salasComTesouro = [2, 4, 7];
+const salasComMonstro = [3, 6, 8];
 
-//TODO: Implemente as condições necessárias para a solução do desafio. Utilize a tabela de exemplos para identificar a escolha do treinador:
-if ( escolhaDoTreinador === 1) {
-    pokemonEscolhido = "Bulbasaur";
-} else if ( escolhaDoTreinador === 2) {
-    pokemonEscolhido = "Charmander";
-} else if ( escolhaDoTreinador === 4) {
-    pokemonEscolhido = "Pikachu";
-} else if ( escolhaDoTreinador === 5){
-    pokemonEscolhido = "Mewtwo";
-}
+//Aqui temos uma forma de loop para percorrer cada sala da masmorra:
+for (let sala = 1; sala <= totalSalas; sala++) {
+//Utilizamos o .includes() para verificar se um número está presente nos arrays:
+    const temTesouro = salasComTesouro.includes(sala);
+    const temMonstro = salasComMonstro.includes(sala);
 
-//Imprime o Pokémon escolhido:
-if (pokemonEscolhido) {
-    print("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
+//TODO: Agora implemente uma condição necessária para verificar e exibir se há tesouro na sala e se há montro na sala:
+    if (temTesouro) {
+        print("Tesouro na sala " + sala + "!");
+    } else if (temMonstro) {
+        print("Monstro na sala " + sala + "!");
+    }
 }
